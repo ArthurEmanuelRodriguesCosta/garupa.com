8f582e0a52b8be5bbec8cf137d82465cab2dabe4
Arthur Costa <arthur.costa@ccc.ufcg.edu.br>
2017-06-29 09:44:30 -0300

    uma coisinha erradaconsertada

--- System Analysis/measurement.py
+++ System Analysis/measurement.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from client import *
-from random import randint
+from random import choice, randint
 import time
 
 auth = Authenticator('http://destroctor51.pythonanywhere.com', '114110495', '12345')
 
-bairros = ['Acácio Figueiredo', 'Alto Branco', 'Bairro das Cidades', 'Bela Vista',
+bairros = [
+    'Acácio Figueiredo', 'Alto Branco', 'Bairro das Cidades', 'Bela Vista',
     'Bodocongó', 'Cachoeira', 'Castelo Branco', 'Catolé',
     'Catolé de Zé Ferreira', 'Centenário', 'Centro', 'Conjunto Cinza',
     'Dinamérica', 'Distrito de Catolé', 'Distrito Industrial', 'Estação Velha',
@@ -22,24 +23,12 @@ bairros = ['Acácio Figueiredo', 'Alto Branco', 'Bairro das Cidades', 'Bela Vist
 
 weekly = [True, False]
 
-
-r = auth.request('POST', '/api/rides', json={
-    'dest': 'UFCG',
-    'weekly': weekly[randint(0, 1)],
-    'origin': bairros[randint(0, len(bairros) - 1)],
-    'route': [bairros[randint(0, len(bairros) - 1)], bairros[randint(0, len(bairros) - 1)],
-              bairros[randint(0, len(bairros) - 1)]],
-    'seats': randint(1, 4),
-    'date': 1498791600000
-})
-
 for i in range(100):
     r = auth.request('POST', '/api/rides', json={
         'dest': 'UFCG',
-        'weekly': weekly[randint(0, 1)],
-        'origin': bairros[randint(0, len(bairros) - 1)],
-        'route': [bairros[randint(0, len(bairros) - 1)], bairros[randint(0, len(bairros) - 1)],
-                  bairros[randint(0, len(bairros) - 1)]],
+        'weekly': choice(weekly),
+        'origin': 'qualquer coisa mano',
+        'route': [choice(bairros), choice(bairros), choice(bairros)],
         'seats': randint(1, 4),
         'date': 1498791600000
     })
