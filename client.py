2953830f69ca6927e4f24d37f98ed974a4ff60c2
Victor Andrade de Almeida <victor.almeida@ccc.ufcg.edu.br>
2017-06-28 22:07:05 -0300

    Método de acesso à API

---
+++ client.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from hashlib import md5
+from os import urandom
+from requests import request
+from urllib import urlencode
+
+class Authenticator(object):
+	realm = None
+	nonce = None
+	cnonce = None
+	nc = 0
+
+	def __init__(self, base_url, user, passwd):
+		self.base_url = base_url
+		self.user = user
+		self.passwd = passwd
+
+	def nc_str(self):
+		return format(self.nc, '016x')
+
+	def gen_nonce(self):
+		return urandom(8).encode('hex')
+
+	def hash_all(self, *args):
+		strings = map(str, args)
+		concat = ':'.join(strings)
+		return md5(concat).hexdigest()
+
+	def make_header(self, method, url, params):
+		if self.realm is None:
+			return None
+
+		uri = url
+		if params is not None:
+			uri += '?' + urlencode(params)
+
+		response = self.hash_all(
+			self.hash_all(self.user, self.realm, self.passwd),
+			self.nonce, self.nc_str(), self.cnonce, 'auth',
+			self.hash_all(method, uri)
+		)
+
+		return {
+			'Authorization':
+				'Digest username=%s, realm=%s, nonce=%s, uri=%s, qop=auth, nc=%s, cnonce=%s, response=%s' %
+				(self.user, self.realm, self.nonce, uri, self.nc_str(), self.cnonce, response)
+		}
+
+	def request(self, method, url, params=None, json=None):
+		headers = self.make_header(method, url, params)
+		self.nc += 1
+
+		r = request(method, self.base_url+url, headers=headers, params=params, json=json)
+
+		if r.status_code is 200:
+			return r
+
+		auth = r.headers['WWW-Authenticate']
+
+		realm = re.search('realm="(.*?)"', auth)
+		nonce = re.search('nonce="(.*?)"', auth)
+		stale = re.search('stale="(.*?)"', auth)
+
+		self.realm = realm.group(1)
+		self.nonce = nonce.group(1)
+		self.cnonce = self.gen_nonce()
+		self.nc = 1
+
+		if stale and stale.group(1) == 'true' or headers == None:
+			return self.request(method, url, params, json)
