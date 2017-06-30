cb8d28dae40ea2cf0d04014f66aaa59d416b5126
Victor Andrade de Almeida <victor.almeida@ccc.ufcg.edu.br>
2017-06-30 02:34:56 -0300

    Janela Deslizante

--- analysis/measurement.py
+++ analysis/measurement.py
@@ -2,7 +2,7 @@
 
 from random import choice, randint, sample
 from sys import stdout
-from threading import Thread, Lock
+from threading import Lock, Semaphore, Thread
 
 from client import *
 
@@ -60,11 +60,9 @@ def gen_ride(query=None):
 
 output = open('out.csv', 'w')
 output.write('queries,results,rides,net_time,process_time,bd_time\n')
-lock = Lock()
 
-def write(string):
-    with lock:
-        output.write(string)
+window = Semaphore(64)
+lock = Lock()
 
 
 class Register(Thread):
@@ -76,6 +74,7 @@ class Register(Thread):
     def run(self):
         r, delta = self.user.request('POST', '/api/rides', json=self.ride)
         stdout.write('.' if r.status_code is 200 else 'x')
+        window.release()
 
 class Search(Thread):
     def __init__(self, user, query, queries, results, rides):
@@ -94,32 +93,31 @@ class Search(Thread):
         if r.status_code == 200:
             json = r.json()
 
-            write('%d,%d,%d,%s,%s,%s\n' % (
-                self.queries, self.results, self.rides,
-                str(delta), str(json['process_time']), str(json['bd_time'])
-            ))
+            with lock:
+                output.write('%d,%d,%d,%s,%s,%s\n' % (
+                    self.queries, self.results, self.rides,
+                    str(delta), str(json['process_time']), str(json['bd_time'])
+                ))
 
-def execute(threads, w=50):
-    i= 0
-    j= -1
-    for i in xrange(len(threads)/w):
-        for j in xrange(w):
-            threads[w*i+j].start()
-        for j in xrange(w):
-            threads[w*i+j].join()
-    for k in xrange(w*i+j+1, len(threads)):
-        threads[k].start()
-    for k in xrange(w*i+j+1, len(threads)):
-        threads[k].join()
+        window.release()
+
+def execute(threads):
+    for t in threads:
+        window.acquire()
+        t.start()
+    for t in threads:
+        t.join()
 
 
-def run_experiment(queries, results, rides):
+def run_experiment(queries, results, rides, rep):
     clear_database()
 
     driver = create_user(114110000)
     passenger = create_user(114110001)
 
     query = gen_query(results)
+
+    print queries, results, rides, rep
 
     stdout.write('rides ')
     threads = []
@@ -149,7 +147,7 @@ def run_experiment(queries, results, rides):
 
 def repeat_experiment(queries, results, rides, reps=1):
     for i in xrange(reps):
-        run_experiment(queries, results, rides)
+        run_experiment(queries, results, rides, i)
 
 
 repeat_experiment(1, 10, 100, 50)
@@ -160,3 +158,5 @@ repeat_experiment(200, 10, 100, 1)
 repeat_experiment(200, 10, 1000, 1)
 repeat_experiment(200, 100, 100, 1)
 repeat_experiment(200, 100, 1000, 1)
+
+output.close()
